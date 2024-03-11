cab1f54b80ea9b0826f1eb1967c872a7cc4db7f3
Juan David Meza Rodriguez <judamerro@gmail.com>
2024-03-11 18:05:17 -0500

    tercer commit

--- C5 Fundamentos Javascript/ejercicios/01.js
+++ C5 Fundamentos Javascript/ejercicios/01.js
@@ -1,6 +1,6 @@
 // Crea una variable de tipo string.
 // Reemplaza el valor de null por el correspondiente.
 
-const nuevoString = 'Juan David Meza Rodrigues';
+const nuevoString = 'Juan David Meza Rodriguez';
 
 module.exports = nuevoString;
