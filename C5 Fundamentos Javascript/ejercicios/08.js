60c4eff60a477b10235d86382ad86f76eefba642
Juan David Meza Rodriguez <judamerro@gmail.com>
2024-04-04 20:14:47 -0500

    Trabajos realizados hasta el 09

--- C5 Fundamentos Javascript/ejercicios/08.js
+++ C5 Fundamentos Javascript/ejercicios/08.js
@@ -3,13 +3,7 @@ function esNumeroEntero(numero) {
   // Verifica si este es un número entero o no.
   // Returna true si lo es, de lo contrario, retorna false.
   // Tu código:
-
-  if (numero  % 1 == 0){
-    console.log(true);
-
-  }else{
-    console.log(false);
-  }
+  return Number.isInteger(numero);
 }
 
 module.exports = esNumeroEntero;
